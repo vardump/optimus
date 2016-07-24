78e9b918efd5cf383e3cbf864df0e20ef5ddba8c
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2016-07-24 20:27:03 +0600

    fixed type on the copyright section

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0
+        <strong>Version</strong> 1.0
     </div>
-    <strong>Copyright &copy; 2016 <a href="http://vardump.net">Var_dump</a>.</strong> All rights reserved.
+    <strong>Developed by </strong><a href="http://vardump.net" target="_blank">vardump</a>
 </footer>
