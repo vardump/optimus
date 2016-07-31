0dfcfdd9b1217380d415532bb1159ea15f0e1ab1
Prappo Prince <prappo.prince@gmail.com>
2016-07-31 18:02:47 +0600

    installer added

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+            RachidLaasri\LaravelInstaller\Providers\LaravelInstallerServiceProvider::class,
         
 
     ],
